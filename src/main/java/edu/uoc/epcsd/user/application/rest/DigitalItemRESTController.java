aa6ad641c90add34ab517a34755a474880709b4f
danicnb <danieltondopaez@gmail.com>
2025-05-19 21:54:28 +0200

    Finish PR2

--- src/main/java/edu/uoc/epcsd/user/application/rest/DigitalItemRESTController.java
+++ src/main/java/edu/uoc/epcsd/user/application/rest/DigitalItemRESTController.java
@@ -1,8 +1,10 @@
 package edu.uoc.epcsd.user.application.rest;
 
 import edu.uoc.epcsd.user.application.rest.request.CreateDigitalItemRequest;
+import edu.uoc.epcsd.user.application.rest.response.GetProductResponse;
 import edu.uoc.epcsd.user.domain.Alert;
 import edu.uoc.epcsd.user.domain.DigitalItem;
+import edu.uoc.epcsd.user.domain.DigitalItemStatus;
 import edu.uoc.epcsd.user.domain.service.DigitalItemService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -33,45 +35,165 @@ public class DigitalItemRESTController {
 
         return digitalItemService.findAllDigitalItem();
     }
-    
-    // TODO: add the code for the missing system operations here: 
+
+    // TODO: add the code for the missing system operations here:
     // use the corresponding mapping HTTP request annotation with the parameter: "/{digitalItemId}"
     // and create the method getDigitalItemById(@PathVariable @NotNull Long digitalItemId)
-    // which call the corresponding getDigitalItemById method 
-    
-    // TODO: add the code for the missing system operations here: 
+    // which call the corresponding getDigitalItemById method
+    @GetMapping("/{digitalItemId}")
+    public ResponseEntity<DigitalItem> getDigitalItemById(@PathVariable @NotNull Long digitalItemId) {
+        log.trace("getDigitalItemById");
+
+        return digitalItemService.getDigitalItemById(digitalItemId).map( digitalItem -> ResponseEntity.ok().body(digitalItem))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    // TODO: add the code for the missing system operations here:
     // use the corresponding mapping HTTP request annotation with the parameter: "/digitalItemBySession"
     // and create the method findDigitalItemBySession(@RequestParam @NotNull Long digitalSessionId)
-    // which call the corresponding findDigitalItemBySession method 
-    
-    // TODO: add the code for the missing system operations here: 
+    // which call the corresponding findDigitalItemBySession method
+    @GetMapping("/digitalItemBySession")
+    @ResponseStatus(HttpStatus.OK)
+    public List<DigitalItem> findDigitalItemBySession(@RequestParam @NotNull Long digitalSessionId) {
+        log.trace("findDigitalItemBySession");
+
+        return digitalItemService.findDigitalItemBySession(digitalSessionId);
+    }
+
+    // TODO: add the code for the missing system operations here:
     // use the corresponding mapping HTTP request annotation with the parameter: "/addItem"
     // and create the method addDigitalItem(@RequestBody @Valid CreateDigitalItemRequest createDigitalItemRequest)
-    // which call the corresponding addDigitalItemm method 
+    // which call the corresponding addDigitalItemm method
 
-    // TODO: add the code for the missing system operations here: 
+    @PostMapping("/addItem")
+    public ResponseEntity<Long> addDigitalItem(@RequestBody @Valid CreateDigitalItemRequest createDigitalItemRequest) {
+        log.trace("addDigitalItem");
+
+        log.trace("createDigitalItemRequest: " + createDigitalItemRequest);
+
+        try {
+            Long digitalItemId = digitalItemService.addDigitalItem(DigitalItem.builder()
+                    .digitalSessionId(createDigitalItemRequest.getDigitalSessionId())
+                    .description(createDigitalItemRequest.getDescription())
+                    .lat(createDigitalItemRequest.getLat())
+                    .lon(createDigitalItemRequest.getLon())
+                    .link(createDigitalItemRequest.getLink())
+                    .status(DigitalItemStatus.AVAILABLE)
+                    .build());
+
+            URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                    .path("/{digitalItemId}")
+                    .buildAndExpand(digitalItemId)
+                    .toUri();
+
+            return ResponseEntity.created(uri).body(digitalItemId);
+
+        } catch (IllegalArgumentException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "The specified digital session " + createDigitalItemRequest.getDigitalSessionId() + " does not exist.", e);
+        }
+    }
+
+    // TODO: add the code for the missing system operations here:
     // use the corresponding mapping HTTP request annotation with the parameter: "/updateItem/{digitalItemId}"
     // and create the method updateDigitalItem(@PathVariable @NotNull Long digitalItemId, @RequestBody @Valid CreateDigitalItemRequest updateDigitalItemRequest)
-    // which call the corresponding updateDigitalItem method 
+    // which call the corresponding updateDigitalItem method
+    @PatchMapping("/updateItem/{digitalItemId}")
+    public ResponseEntity<Long> updateDigitalItem(@PathVariable @NotNull Long digitalItemId, @RequestBody @Valid CreateDigitalItemRequest updateDigitalItemRequest) {
+        log.trace("updateDigitalItem");
 
-    // TODO: add the code for the missing system operations here: 
+        log.trace("Updating digital item " + digitalItemId);
+
+        try {
+            Long id = digitalItemService.updateDigitalItem(digitalItemId,
+                    updateDigitalItemRequest.getDescription(),
+                    updateDigitalItemRequest.getLink(),
+                    updateDigitalItemRequest.getLat(),
+                    updateDigitalItemRequest.getLon());
+
+            URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                    .path("/{id}")
+                    .buildAndExpand(id)
+                    .toUri();
+
+            return ResponseEntity.created(uri).body(id);
+
+        } catch (IllegalArgumentException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "The specified digital item " + digitalItemId + " does not exist.", e);
+        }
+    }
+
+    // TODO: add the code for the missing system operations here:
     // use the corresponding mapping HTTP request annotation with the parameter: "/reviewDigitalItem/{digitalItemId}"
     // and create the method setDigitalItemForReview(@PathVariable @NotNull Long digitalItemId)
-    // which call the corresponding setDigitalItemForReview method     
-    
-    // TODO: add the code for the missing system operations here: 
+    // which call the corresponding setDigitalItemForReview method
+    @PatchMapping("/reviewDigitalItem/{digitalItemId}")
+    @ResponseStatus(HttpStatus.OK)
+    public void setDigitalItemForReview(@PathVariable @NotNull Long digitalItemId) {
+        log.trace("setDigitalItemForReview");
+
+        log.trace("Reviewing digital item " + digitalItemId);
+
+        try {
+            digitalItemService.setDigitalItemForReview(digitalItemId);
+
+        } catch (IllegalArgumentException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "The specified digital item " + digitalItemId + " does not exist.", e);
+        }
+    }
+
+    // TODO: add the code for the missing system operations here:
     // use the corresponding mapping HTTP request annotation with the parameter: "/approveDigitalItem/{digitalItemId}"
     // and create the method approvePendingDigitalItem(@PathVariable @NotNull Long digitalItemId)
-    // which call the corresponding approvePendingDigitalItem method     
-    
-    // TODO: add the code for the missing system operations here: 
+    // which call the corresponding approvePendingDigitalItem method
+    @PatchMapping("/approveDigitalItem/{digitalItemId}")
+    @ResponseStatus(HttpStatus.OK)
+    public void approvePendingDigitalItem(@PathVariable @NotNull Long digitalItemId) {
+        log.trace("approvePendingDigitalItem");
+
+        log.trace("Approving digital item " + digitalItemId);
+
+        try{
+            digitalItemService.approvePendingDigitalItem(digitalItemId);
+
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException(e.getMessage());
+        }
+    }
+
+    // TODO: add the code for the missing system operations here:
     // use the corresponding mapping HTTP request annotation with the parameter: "/rejectDigitalItem/{digitalItemId}"
     // and create the method rejectPendingDigitalItem(@PathVariable @NotNull Long digitalItemId)
-    // which call the corresponding rejectPendingDigitalItem method    
-    
+    // which call the corresponding rejectPendingDigitalItem method
+    @PatchMapping("/rejectDigitalItem/{digitalItemId}")
+    @ResponseStatus(HttpStatus.OK)
+    public void rejectPendingDigitalItem(@PathVariable @NotNull Long digitalItemId) {
+        log.trace("rejectPendingDigitalItem");
+
+        log.trace("Rejecting digital item " + digitalItemId);
+
+        try{
+            digitalItemService.rejectPendingDigitalItem(digitalItemId);
+
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException(e.getMessage());
+        }
+    }
+
     // TODO: add the code for the missing system operations here: 
     // use the corresponding mapping HTTP request annotation with the parameter: "/dropItem/{digitalItemId}"
     // and create the method dropDigitalItem(@PathVariable @NotNull Long digitalItemId)
-    // which call the corresponding dropDigitalItem method 
-  
+    // which call the corresponding dropDigitalItem method
+    @DeleteMapping("/dropItem/{digitalItemId}")
+    public ResponseEntity<Void> dropDigitalItem(@PathVariable @NotNull Long digitalItemId) {
+        log.trace("dropDigitalItem");
+        log.trace("Deleting digital item " + digitalItemId);
+
+        digitalItemService.getDigitalItemById(digitalItemId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                        "The specified digital item " + digitalItemId + " does not exist."));
+
+        digitalItemService.dropDigitalItem(digitalItemId);
+
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/edu/uoc/epcsd/user/application/rest/DigitalSessionRESTController.java
+++ src/main/java/edu/uoc/epcsd/user/application/rest/DigitalSessionRESTController.java
@@ -2,6 +2,7 @@ package edu.uoc.epcsd.user.application.rest;
 
 import edu.uoc.epcsd.user.application.rest.request.CreateDigitalSessionRequest;
 import edu.uoc.epcsd.user.domain.DigitalItem;
+import edu.uoc.epcsd.user.domain.DigitalItemStatus;
 import edu.uoc.epcsd.user.domain.DigitalSession;
 import edu.uoc.epcsd.user.domain.service.DigitalSessionService;
 import lombok.RequiredArgsConstructor;
@@ -37,26 +38,100 @@ public class DigitalSessionRESTController {
     // TODO: add the code for the missing system operations here: 
     // use the corresponding mapping HTTP request annotation with the parameter: "/{digitalSessionId}"
     // and create the method getDigitalSessionById(@PathVariable @NotNull Long digitalSessionId)
-    // which call the corresponding getDigitalSessionById method 
+    // which call the corresponding getDigitalSessionById method
+    @GetMapping("/{digitalSessionId}")
+    public ResponseEntity<DigitalSession> getDigitalSessionById(@PathVariable @NotNull Long digitalSessionId) {
+        log.trace("getDigitalSessionById");
+
+        return digitalSessionService.getDigitalSessionById(digitalSessionId).map( digitalItem -> ResponseEntity.ok().body(digitalItem))
+                .orElse(ResponseEntity.notFound().build());
+    }
 
     // TODO: add the code for the missing system operations here: 
     // use the corresponding mapping HTTP request annotation with the parameter: "/digitalByUser"
     // and create the method findDigitalSessionByUser(@RequestParam @NotNull Long userId)
-    // which call the corresponding findDigitalSessionByUser method 
-       
+    // which call the corresponding findDigitalSessionByUser method
+    @GetMapping("/digitalByUser")
+    public List<DigitalSession> findDigitalSessionByUser(@RequestParam @NotNull Long userId) {
+        log.trace("findDigitalSessionByUser");
+
+        return digitalSessionService.findDigitalSessionByUser(userId);
+    }
+
     // TODO: add the code for the missing system operations here: 
     // use the corresponding mapping HTTP request annotation with the parameter: "/createDigital"
     // and create the method createDigitalSession(@RequestBody @Valid CreateDigitalSessionRequest createDigitalSessionRequest)
-    // which call the corresponding createDigitalSession method 
+    // which call the corresponding createDigitalSession method
+    @PostMapping("/createDigital")
+    public ResponseEntity<Long> createDigitalSession(@RequestBody @Valid CreateDigitalSessionRequest createDigitalSessionRequest) {
+        log.trace("createDigitalSession");
+
+        log.trace("Creating digital session" + createDigitalSessionRequest);
+
+        try {
+            Long digitalSessionId = digitalSessionService.createDigitalSession(DigitalSession.builder()
+                    .description(createDigitalSessionRequest.getDescription())
+                    .location(createDigitalSessionRequest.getLocation())
+                    .link(createDigitalSessionRequest.getLink())
+                    .userId(createDigitalSessionRequest.getUserId())
+                    .build());
+
+            URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                    .path("/{createDigital}")
+                    .buildAndExpand(digitalSessionId)
+                    .toUri();
+
+            return ResponseEntity.created(uri).body(digitalSessionId);
+
+        } catch (IllegalArgumentException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "The specified user " + createDigitalSessionRequest.getUserId() + " does not exist.", e);
+        }
+    }
 
     // TODO: add the code for the missing system operations here: 
     // use the corresponding mapping HTTP request annotation with the parameter: "/updateDigital/{digitalSessionId}"
     // and create the method updateDigitalSession(@PathVariable @NotNull Long digitalSessionId, @RequestBody @Valid CreateDigitalSessionRequest updateDigitalSessionRequest)
-    // which call the corresponding updateDigitalSession method 
+    // which call the corresponding updateDigitalSession method
+    @PatchMapping("/updateDigital/{digitalSessionId}")
+    public ResponseEntity<Long> updateDigitalSession(@PathVariable @NotNull Long digitalSessionId, @RequestBody @Valid CreateDigitalSessionRequest updateDigitalSessionRequest) {
+        log.trace("updateDigitalSession");
+
+        log.trace("Updating digital session" + updateDigitalSessionRequest);
+
+        try {
+            Long id = digitalSessionService.updateDigitalSession(digitalSessionId,
+                    updateDigitalSessionRequest.getDescription(),
+                    updateDigitalSessionRequest.getLink(),
+                    updateDigitalSessionRequest.getLocation(),
+                    updateDigitalSessionRequest.getUserId());
+
+            URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                    .path("/{id}")
+                    .buildAndExpand(id)
+                    .toUri();
+
+            return ResponseEntity.created(uri).body(id);
+
+        } catch (IllegalArgumentException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "The specified digital session " + digitalSessionId + " does not exist.", e);
+        }
+    }
 
     // TODO: add the code for the missing system operations here: 
     // use the corresponding mapping HTTP request annotation with the parameter: "/removeDigital/{digitalSessionId}"
     // and create the method removeDigitalSession(@PathVariable @NotNull Long digitalSessionId
-    // which call the corresponding removeDigitalSession method 
+    // which call the corresponding removeDigitalSession method
+    @DeleteMapping("/removeDigital/{digitalSessionId}")
+    public ResponseEntity<Long> removeDigitalSession(@PathVariable @NotNull Long digitalSessionId) {
+        log.trace("removeDigitalSession");
 
+        log.trace("Removing digital session" + digitalSessionId);
+
+        digitalSessionService.getDigitalSessionById(digitalSessionId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "The specified digital session " + digitalSessionId + " does not exist."));
+
+        digitalSessionService.removeDigitalSession(digitalSessionId);
+
+        return ResponseEntity.noContent().build();
+    }
 }
